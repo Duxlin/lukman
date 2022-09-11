2b7765bfe974ef29d8e967508148af55f25c8e6a
Duxlin <96833294+Duxlin@users.noreply.github.com>
2022-09-11 02:57:22 +0100

    Config.js

---
+++ Config.js
@@ -0,0 +1,25 @@
+{
+	global.owner = ['2347082252014'] #nomer mu tod
+global.premium = ['2347082252014'] #nomer mu
+global.ownername = 'Lukman XD'
+global.packname = 'Lukman XD??'
+global.author = 'WhatsApp 👿'
+global.sessionName = 'CyberExe'
+global.linkgrupss = ' https://chat.whatsapp.com/LoLb0Rdvnlj6qpsr2wtZvT'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Donee',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Espere a que su solicitud sea procesada',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
